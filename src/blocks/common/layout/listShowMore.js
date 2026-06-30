d33b063cafa45239ad99650f4f1677297c9bbe8a
andre <andre@metasolutions.se>
2026-06-30 09:38:13 +0200

    support vertical in listShowMore

--- src/blocks/common/layout/listShowMore.js
+++ src/blocks/common/layout/listShowMore.js
@@ -1,17 +1,18 @@
 /*
 Todo:
 * replace pagination by a  "show all" button which links
-  to the search page, filtered on the terminology.
-  Blocked by BLOCKS-440
+* to the search page, filtered on the terminology.
+* Blocked by BLOCKS-440
 */
 export default {
   extends: 'list',
   layout: 'raw',
   semanticHtml: true,
+  vertical: false,
   namedclick: '',
   limit: 'inherit',
   bodyClass: '',
   rowClass: '',
-  listbody: '<div class="esbInlineList{{#if bodyClass}} {{bodyClass}}{{/if}}">{{body}}</div>',
+  listbody: '<div class="esbInlineList{{#if vertical}} esbInlineVerticalList{{/if}}{{#if bodyClass}} {{bodyClass}}{{/if}}">{{body}}</div>',
   rowhead: `{{link namedclick="inherit" class=rowClass}}`,
 };
